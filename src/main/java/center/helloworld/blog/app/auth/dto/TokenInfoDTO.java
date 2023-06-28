d799a6c7c3c715bd05bbbc0a7684bebd0d9ce896
“damoncai” <782099197@qq.com>
2023-06-28 15:22:58 +0800

    修改属性名称

--- src/main/java/center/helloworld/blog/app/auth/dto/TokenInfoDTO.java
+++ src/main/java/center/helloworld/blog/app/auth/dto/TokenInfoDTO.java
@@ -5,12 +5,12 @@ import lombok.Data;
 @Data
 public class TokenInfoDTO {
 
-    private String tokeName;
+    private String tokenName;
 
     private String tokenValue;
 
-    public TokenInfoDTO(String tokeName, String tokenValue) {
-        this.tokeName = tokeName;
+    public TokenInfoDTO(String tokenName, String tokenValue) {
+        this.tokenName = tokenName;
         this.tokenValue = tokenValue;
     }
 }
